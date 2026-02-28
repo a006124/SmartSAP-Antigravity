b6644a40ca841925df7a13ec0503b7066b6a38cf
a006124 <93441431+a006124@users.noreply.github.com>
2026-02-28 12:53:38 +0100

    Add files via upload

---
+++ Views/LibraryView.xaml.vb
@@ -0,0 +1,9 @@
+Namespace SmartSAP.Views
+    Partial Public Class LibraryView
+        Inherits System.Windows.Controls.UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
---
+++ Views/ModuleDetailView.xaml.vb
@@ -0,0 +1,9 @@
+Namespace SmartSAP.Views
+    Partial Public Class ModuleDetailView
+        Inherits System.Windows.Controls.UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
